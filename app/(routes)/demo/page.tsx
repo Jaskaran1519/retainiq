84b6c2761ca06da7e3c983954d0cd528d6974bd5
Jaskaran1519 <jaskaran1519@gmail.com>
2024-07-28 15:30:29 +0530

    sdfbsdf

--- app/(routes)/demo/page.tsx
+++ app/(routes)/demo/page.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/app/(components)/Footer";
 import ProductGrid from "@/app/(components)/ProductGrid";
-import { ArrowBigLeft, ArrowLeft, ArrowLeftIcon } from "lucide-react";
+import { ArrowBigLeft, ArrowLeft, ArrowLeftIcon, Github } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -8,13 +8,20 @@ const demo = () => {
   return (
     <div className="w-full h-auto">
       <div className="flex justify-between items-center p-5 mx-5 mb-10">
-        <div className="flex gap-5 items-center">
-          <ArrowLeftIcon size={40} />
+        <Link href="/">
+          <div className="flex gap-5 items-center">
+            <ArrowLeftIcon size={40} />
 
-          <Link href="/">
             <h1 className="font-semibold text-3xl">Back</h1>
-          </Link>
-        </div>
+          </div>
+        </Link>
+        <a
+          href="https://github.com/Jaskaran1519/retainiq"
+          target="_blank"
+          className="p-2 border bg-gray-50 rounded-full"
+        >
+          <Github />
+        </a>
         <button className="bg-green-500 text-white text-md font-semibold py-2 px-4 rounded-xl">
           <h1>Publish feed</h1>
         </button>
